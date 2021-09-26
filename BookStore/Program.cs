a282008f26b3b5e7257dac6b0e5a710cd89f690c
vlapey <dec1nepidor@gmail.com>
2021-09-26 19:22:52 +0300

    Initial commit

---
+++ BookStore/Program.cs
@@ -0,0 +1,27 @@
+﻿
+/*
+ Раз уж такая пьянка пошла, но замутим прилагу магазина книг
+ Чтобы функционал был следующий: 
+ просмотр списка книг, 
+ добавление определённых в корзину, 
+ просмотр корзины, 
+ сортировка по цене вверх и вниз, 
+ поиск по названию
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Threading.Channels;
+
+namespace BookStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
+    
