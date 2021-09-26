c229cc5188a612a81afa719a616b59bc238dcbd6
vlapey <dec1nepidor@gmail.com>
2021-09-26 22:21:33 +0300

    Replaced cases with functions and made some exceptions

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -6,11 +6,14 @@
  case 3:просмотр корзины, 
  case 4 and 5: сортировка по цене вверх и вниз, 
  case 6: поиск по названию
+ создать отдельную функцию на вывод
+ 
  */
 
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
+using System.Linq;
 using System.Threading.Channels;
 
 namespace BookStore
@@ -30,52 +33,97 @@ namespace BookStore
             };
             while (true)
             {
-                Console.WriteLine("Choose action:\n" +
+                Console.WriteLine("\nChoose action:\n" +
                                   "1 - Look at the list of books\n" +
                                   "2 - Add a book to cart\n" +
                                   "3 - Look at the list of books in cart\n" +
                                   "4 - Sorting up by price\n" +
                                   "5 - Sorting down by price\n" +
-                                  "6 - Find book by name\n");
+                                  "6 - Find book by name");
                 var selector = int.Parse(Console.ReadLine());
 
                 switch (selector)
                 {
                     case 1:
-                        foreach (var book in books)
-                        {
-                            Console.WriteLine(book);
-                        }
-
+                        PrintBookList(books);
                         break;
+                    
                     case 2:
-                        foreach (var book in books)
-                        {
-                            Console.WriteLine(book);
-                        }
-                        Console.WriteLine("Type the name of the book that you want to add to cart");
-                        string bookNameForAdd = Console.ReadLine();
-                        Cart.bookList.Add(books.Find(book => book.Name == bookNameForAdd));
+                        PrintBookList(books);
+                        AddBook(books);
                         break;
+                    
                     case 3: 
-                        foreach (var book in Cart.bookList)
-                        {
-                            Console.WriteLine(book);
-                        }
+                        PrintCartList(books);
                         break;
-                    case 4: Cart.bookList.Sort((book1, book2) => book1.Price - book2.Price);
+                    
+                    case 4: AscendingSort(books);
                         break;
-                    case 5: Cart.bookList.Sort((book1, book2) => book2.Price - book1.Price);
+                    
+                    case 5: DescendingSort(books);
                         break;
-                    case 6:  Console.WriteLine("Type the name of the book that you want to find in the store");
-                        string bookNameForFindAll = Console.ReadLine();
-                        Book foundedBook = books.Find(book => bookNameForFindAll == book.Name);
-                        Console.WriteLine(foundedBook);
+                    
+                    case 6:  FindBook(books);
                         break;
+                    
                     default: Console.WriteLine("You've chosen wrong action");
                         break;
                 }
             }
+        }
+
+        private static void PrintBookList(List<Book> books)
+        {
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+        private static void PrintCartList(List<Book> bookList)
+        {
+            if (Cart.bookList.Count == 0)
+                Console.WriteLine("Cart is empty");
+            else 
+            {
+                foreach (var book in Cart.bookList)
+                {
+                    Console.WriteLine(book);
+                }
+            }
+        }
+        private static void AddBook(List<Book> books)
+        {
+            Console.WriteLine("Type the name of the book that you want to add to cart");
+            string bookNameForAdd = Console.ReadLine();
+            try
+            {
+                Book bookToAdd = books.First(book => book.Name == bookNameForAdd);
+                Cart.bookList.Add(bookToAdd);
+                Console.WriteLine("Book added");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Book was not added");
+            }
+        }
+        private static void AscendingSort(List<Book> books)
+        {
+            books.Sort((book1, book2) => book1.Price - book2.Price);
+            Console.WriteLine("Sorted up by price");
+        }
+        private static void DescendingSort(List<Book> books)
+        {
+            books.Sort((book1, book2) => book2.Price - book1.Price);
+            Console.WriteLine("Sorted up by price");
+        }
+        private static void FindBook(List<Book> books)
+        {
+            Console.WriteLine("Type the name of the book that you want to find in the store");
+            string bookNameForFindAll = Console.ReadLine();
+            Book foundedBook = books.Find(book => bookNameForFindAll == book.Name);
+            if (foundedBook == null)
+                Console.WriteLine("Book is not found");
+            else Console.WriteLine(foundedBook);
         }
     }
 }
