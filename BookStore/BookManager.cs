d7af8e08c8a62b95bf4a11fd6c8066131706a2a8
vlapey <dec1nepidor@gmail.com>
2021-09-26 23:56:01 +0300

    Added class Book manager and moved all the functions to this one

---
+++ BookStore/BookManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace BookStore
+{
+    public class BookManager
+    {
+       public static List<Book> books = new List<Book>()
+        {
+            new Book() {Price = 30, Name = "Game of thrones"},
+            new Book() {Price = 13, Name = "Jack of all trades"},
+            new Book() {Price = 20, Name = "Alice in the wonderland"},
+            new Book() {Price = 15, Name = "Through the eyes of the victim"},
+            new Book() {Price = 22, Name = "1984"}
+        };
+        public static void PrintBookList(List<Book> books)
+        {
+            foreach (var book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+        public static void PrintCartList(List<Book> bookList)
+        {
+            if (Cart.bookList.Count == 0)
+                Console.WriteLine("Cart is empty");
+            else 
+            {
+                foreach (var book in Cart.bookList)
+                {
+                    Console.WriteLine(book);
+                }
+            }
+        }
+        public static void AddBook(List<Book> books)
+        {
+            Console.WriteLine("Type the name of the book that you want to add to cart");
+            string bookNameForAdd = Console.ReadLine();
+            try
+            {
+                Book bookToAdd = books.First(book => book.Name == bookNameForAdd);
+                Cart.bookList.Add(bookToAdd);
+                Console.WriteLine("Book added");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Book was not added");
+            }
+        }
+        public static void AscendingSort(List<Book> books)
+        {
+            books.Sort((book1, book2) => book1.Price - book2.Price);
+            Console.WriteLine("Sorted up by price");
+        }
+        public static void DescendingSort(List<Book> books)
+        {
+            books.Sort((book1, book2) => book2.Price - book1.Price);
+            Console.WriteLine("Sorted up by price");
+        }
+        public static void FindBook(List<Book> books)
+        {
+            Console.WriteLine("Type the name of the book that you want to find in the store");
+            string bookNameForFindAll = Console.ReadLine();
+            Book foundedBook = books.Find(book => bookNameForFindAll == book.Name);
+            if (foundedBook == null)
+                Console.WriteLine("Book is not found");
+            else Console.WriteLine(foundedBook);
+        }
+        
+    }
+}
--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -22,15 +22,6 @@ namespace BookStore
     {
         static void Main(string[] args)
         {
-            List<Book> books = new List<Book>()
-            {
-                new Book() {Price = 30, Name = "Game of thrones"},
-                new Book() {Price = 13, Name = "Jack of all trades"},
-                new Book() {Price = 20, Name = "Alice in the wonderland"},
-                new Book() {Price = 15, Name = "Through the eyes of the victim"},
-                new Book() {Price = 22, Name = "1984"}
-                
-            };
             while (true)
             {
                 Console.WriteLine("\nChoose action:\n" +
@@ -41,29 +32,29 @@ namespace BookStore
                                   "5 - Sorting down by price\n" +
                                   "6 - Find book by name");
                 var selector = int.Parse(Console.ReadLine());
-
+                BookManager books = new BookManager();
                 switch (selector)
                 {
                     case 1:
-                        PrintBookList(books);
+                       BookManager.PrintBookList(BookManager.books);
                         break;
                     
                     case 2:
-                        PrintBookList(books);
-                        AddBook(books);
+                        BookManager.PrintBookList(BookManager.books);
+                        BookManager.AddBook(BookManager.books);
                         break;
                     
                     case 3: 
-                        PrintCartList(books);
+                        BookManager.PrintCartList(BookManager.books);
                         break;
                     
-                    case 4: AscendingSort(books);
+                    case 4: BookManager.AscendingSort(BookManager.books);
                         break;
                     
-                    case 5: DescendingSort(books);
+                    case 5: BookManager.DescendingSort(BookManager.books);
                         break;
                     
-                    case 6:  FindBook(books);
+                    case 6:  BookManager.FindBook(BookManager.books);
                         break;
                     
                     default: Console.WriteLine("You've chosen wrong action");
@@ -72,59 +63,7 @@ namespace BookStore
             }
         }
 
-        private static void PrintBookList(List<Book> books)
-        {
-            foreach (var book in books)
-            {
-                Console.WriteLine(book);
-            }
-        }
-        private static void PrintCartList(List<Book> bookList)
-        {
-            if (Cart.bookList.Count == 0)
-                Console.WriteLine("Cart is empty");
-            else 
-            {
-                foreach (var book in Cart.bookList)
-                {
-                    Console.WriteLine(book);
-                }
-            }
-        }
-        private static void AddBook(List<Book> books)
-        {
-            Console.WriteLine("Type the name of the book that you want to add to cart");
-            string bookNameForAdd = Console.ReadLine();
-            try
-            {
-                Book bookToAdd = books.First(book => book.Name == bookNameForAdd);
-                Cart.bookList.Add(bookToAdd);
-                Console.WriteLine("Book added");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Book was not added");
-            }
-        }
-        private static void AscendingSort(List<Book> books)
-        {
-            books.Sort((book1, book2) => book1.Price - book2.Price);
-            Console.WriteLine("Sorted up by price");
-        }
-        private static void DescendingSort(List<Book> books)
-        {
-            books.Sort((book1, book2) => book2.Price - book1.Price);
-            Console.WriteLine("Sorted up by price");
-        }
-        private static void FindBook(List<Book> books)
-        {
-            Console.WriteLine("Type the name of the book that you want to find in the store");
-            string bookNameForFindAll = Console.ReadLine();
-            Book foundedBook = books.Find(book => bookNameForFindAll == book.Name);
-            if (foundedBook == null)
-                Console.WriteLine("Book is not found");
-            else Console.WriteLine(foundedBook);
-        }
+       
     }
 }
     
