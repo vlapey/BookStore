115a5a35962883443b86f81f5a4c538d8af14153
vlapey <dec1nepidor@gmail.com>
2021-09-27 19:03:06 +0300

    created class User

---
+++ BookStore/User.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStore
+{
+    public class User
+    {
+        private string Login { get; set; }
+        private string NickName { get; set; }
+        private string Password { get; set; }
+        
+    }
+}
