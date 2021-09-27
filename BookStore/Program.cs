9f460b789f56e2416071dfbb357b5b95b57c37d2
vlapey <dec1nepidor@gmail.com>
2021-09-27 19:56:56 +0300

    Introduced login/password system, added user 1

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -22,47 +22,72 @@ namespace BookStore
     {
         static void Main(string[] args)
         {
-            while (true)
+            User user1 = new User()
             {
-                Console.WriteLine("\nChoose action:\n" +
-                                  "1 - Look at the list of books\n" +
-                                  "2 - Add a book to cart\n" +
-                                  "3 - Look at the list of books in cart\n" +
-                                  "4 - Sorting up by price\n" +
-                                  "5 - Sorting down by price\n" +
-                                  "6 - Find book by name");
-                var selector = int.Parse(Console.ReadLine());
-                switch (selector)
+                Login = "aleksLeonov",
+                Password = "5963"
+            };
+            User user2 = new User();
+            User user3 = new User();
+            {
+                while (true)
                 {
-                    case 1:
-                       BookManager.PrintBookList();
-                        break;
-                    
-                    case 2:
-                        BookManager.PrintBookList();
-                        BookManager.AddBook();
-                        break;
-                    
-                    case 3: 
-                        BookManager.PrintCartList();
-                        break;
-                    
-                    case 4: BookManager.AscendingSort();
-                        break;
-                    
-                    case 5: BookManager.DescendingSort();
-                        break;
-                    
-                    case 6:  BookManager.FindBook();
-                        break;
-                    
-                    default: Console.WriteLine("You've chosen wrong action");
-                        break;
+                    Console.WriteLine("Please enter your login");
+                    var compareLogin = Console.ReadLine();
+                    if (compareLogin == user1.Login)
+                    {
+                        Console.WriteLine("Please enter your password");
+                        var comparePassword = Console.ReadLine();
+                        if (comparePassword == user1.Password)
+                        {
+                            Console.WriteLine("\nChoose action:\n" +
+                                              "1 - Look at the list of books\n" +
+                                              "2 - Add a book to cart\n" +
+                                              "3 - Look at the list of books in cart\n" +
+                                              "4 - Sorting up by price\n" +
+                                              "5 - Sorting down by price\n" +
+                                              "6 - Find book by name");
+                            var selector = int.Parse(Console.ReadLine());
+                            switch (selector)
+                            {
+                                case 1:
+                                    BookManager.PrintBookList();
+                                    break;
+
+                                case 2:
+                                    BookManager.PrintBookList();
+                                    BookManager.AddBook();
+                                    break;
+
+                                case 3:
+                                    BookManager.PrintCartList();
+                                    break;
+
+                                case 4:
+                                    BookManager.AscendingSort();
+                                    break;
+
+                                case 5:
+                                    BookManager.DescendingSort();
+                                    break;
+
+                                case 6:
+                                    BookManager.FindBook();
+                                    break;
+
+                                default:
+                                    Console.WriteLine("You've chosen wrong action");
+                                    break;
+                            }
+                        }
+                        else Console.WriteLine("You've entered wrong password");
+                    }
+                    else Console.WriteLine("You've entered wrong login");
                 }
             }
+            
         }
-
-       
+        
     }
 }
     
--- BookStore/User.cs
+++ BookStore/User.cs
@@ -2,9 +2,9 @@
 {
     public class User
     {
-        private string Login { get; set; }
-        private string NickName { get; set; }
-        private string Password { get; set; }
+        public string Login { get; set; }
         
+        public string Password { get; set; }
+        public string NickName { get; set; }
     }
 }
