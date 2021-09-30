b496151248d47bf5d137facc658cc5afcf09540f
vlapey <dec1nepidor@gmail.com>
2021-09-30 19:53:35 +0300

    Fixed user class

--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -1,16 +1,4 @@
-﻿
-/*
- Чтобы функционал был следующий: 
- case 1: просмотр списка книг, 
- case 2: добавление определённых в корзину, 
- case 3:просмотр корзины, 
- case 4 and 5: сортировка по цене вверх и вниз, 
- case 6: поиск по названию
- создать отдельную функцию на вывод
- 
- */
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Linq;
@@ -22,72 +10,58 @@ namespace BookStore
     {
         static void Main(string[] args)
         {
-            User user1 = new User()
-            {
-                Login = "aleksLeonov",
-                Password = "5963"
-            };
-            User user2 = new User();
-            User user3 = new User();
-            {
-                while (true)
+            Console.WriteLine("Please enter your login");
+                var compareLogin = Console.ReadLine();
+                Console.WriteLine("Please enter your password");
+                var comparePassword = Console.ReadLine();
+                if (User.users.Exists(user => user.Login == compareLogin && user.Password == comparePassword) == true)
                 {
-                    Console.WriteLine("Please enter your login");
-                    var compareLogin = Console.ReadLine();
-                    if (compareLogin == user1.Login)
+                    while (true)
                     {
-                        Console.WriteLine("Please enter your password");
-                        var comparePassword = Console.ReadLine();
-                        if (comparePassword == user1.Password)
+                        Console.WriteLine("\nChoose action:\n" +
+                                          "1 - Look at the list of books\n" +
+                                          "2 - Add a book to cart\n" +
+                                          "3 - Look at the list of books in cart\n" +
+                                          "4 - Sorting up by price\n" +
+                                          "5 - Sorting down by price\n" +
+                                          "6 - Find book by name");
+                        var selector = int.Parse(Console.ReadLine());
+                        switch (selector)
                         {
-                            Console.WriteLine("\nChoose action:\n" +
-                                              "1 - Look at the list of books\n" +
-                                              "2 - Add a book to cart\n" +
-                                              "3 - Look at the list of books in cart\n" +
-                                              "4 - Sorting up by price\n" +
-                                              "5 - Sorting down by price\n" +
-                                              "6 - Find book by name");
-                            var selector = int.Parse(Console.ReadLine());
-                            switch (selector)
-                            {
-                                case 1:
-                                    BookManager.PrintBookList();
-                                    break;
+                            case 1:
+                                BookManager.PrintBookList();
+                                break;
 
-                                case 2:
-                                    BookManager.PrintBookList();
-                                    BookManager.AddBook();
-                                    break;
+                            case 2:
+                                BookManager.PrintBookList();
+                                BookManager.AddBook();
+                                break;
 
-                                case 3:
-                                    BookManager.PrintCartList();
-                                    break;
+                            case 3:
+                                BookManager.PrintCartList();
+                                break;
 
-                                case 4:
-                                    BookManager.AscendingSort();
-                                    break;
+                            case 4:
+                                BookManager.AscendingSort();
+                                break;
 
-                                case 5:
-                                    BookManager.DescendingSort();
-                                    break;
+                            case 5:
+                                BookManager.DescendingSort();
+                                break;
 
-                                case 6:
-                                    BookManager.FindBook();
-                                    break;
+                            case 6:
+                                BookManager.FindBook();
+                                break;
 
-                                default:
-                                    Console.WriteLine("You've chosen wrong action");
-                                    break;
-                            }
+                            default:
+                                Console.WriteLine("You've chosen wrong action");
+                                break;
                         }
-                        else Console.WriteLine("You've entered wrong password");
                     }
-                    else Console.WriteLine("You've entered wrong login");
                 }
+                Console.WriteLine("Incorrect login or password");
             }
-            
+
         }
         
     }
-}
-    
--- BookStore/User.cs
+++ BookStore/User.cs
@@ -1,10 +1,19 @@
-﻿namespace BookStore
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace BookStore
 {
     public class User
     {
-        public string Login { get; set; }
-        
-        public string Password { get; set; }
-        public string NickName { get; set; }
+        public string Login;
+        public string Password;
+        public static List<User> users = new()
+        {
+            new User(){Login = "alekseyleonov", Password = "5963"},
+            new User(){Login = "vlapey", Password = "2563"},
+            new User(){Login = "vkondrashkov", Password = "1452"},
+            new User(){Login = "armatura", Password = "8569"},
+        };
     }
 }
