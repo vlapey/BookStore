cd94c8ea9dab1eb41d352b0b379cab3514e011fd
vlapey <dec1nepidor@gmail.com>
2021-10-08 12:29:14 +0300

    Added database functionality

---
+++ BookStore/DataBase.cs
@@ -0,0 +1,44 @@
+﻿using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookStore
+{
+    public class DataBase
+    {
+        private static MySqlConnection connection = new("server=localhost;port=3306;" +
+                                                        "username=root;password=root;database=bookstore");
+
+        public static void OpenConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+        }
+
+        public static void CloseConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+        }
+        
+        public static List<string[]> GetUsers()
+        {
+            OpenConnection();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users`", connection);
+            /*command.ExecuteNonQuery(); Вариант для insert и для delete*/
+            List<string[]> users = new List<string[]>();
+            MySqlDataReader dataReader = command.ExecuteReader();
+            while (dataReader.Read())
+            {
+                users.Add(new string[3]);
+                for (int i = 0; i < 3; i++)
+                {
+                    users.Last()[i] = dataReader[i].ToString();
+                }
+            }
+            dataReader.Close();
+            CloseConnection();
+            return users;
+        }
+    }
+}
--- BookStore/Program.cs
+++ BookStore/Program.cs
@@ -10,58 +10,13 @@ namespace BookStore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter your login");
-                var compareLogin = Console.ReadLine();
-                Console.WriteLine("Please enter your password");
-                var comparePassword = Console.ReadLine();
-                if (User.users.Exists(user => user.Login == compareLogin && user.Password == comparePassword) == true)
-                {
-                    while (true)
-                    {
-                        Console.WriteLine("\nChoose action:\n" +
-                                          "1 - Look at the list of books\n" +
-                                          "2 - Add a book to cart\n" +
-                                          "3 - Look at the list of books in cart\n" +
-                                          "4 - Sorting up by price\n" +
-                                          "5 - Sorting down by price\n" +
-                                          "6 - Find book by name");
-                        var selector = int.Parse(Console.ReadLine());
-                        switch (selector)
-                        {
-                            case 1:
-                                BookManager.PrintBookList();
-                                break;
-
-                            case 2:
-                                BookManager.PrintBookList();
-                                BookManager.AddBook();
-                                break;
-
-                            case 3:
-                                BookManager.PrintCartList();
-                                break;
-
-                            case 4:
-                                BookManager.AscendingSort();
-                                break;
-
-                            case 5:
-                                BookManager.DescendingSort();
-                                break;
-
-                            case 6:
-                                BookManager.FindBook();
-                                break;
-
-                            default:
-                                Console.WriteLine("You've chosen wrong action");
-                                break;
-                        }
-                    }
-                }
-                Console.WriteLine("Incorrect login or password");
-            }
-
+            DataBase.OpenConnection();
+            DataBase.CloseConnection();
+            /*foreach (var user in DataBase.GetUsers())
+            {
+                Console.WriteLine($"Userlogin is {user[1]} and password is {user[2]}");
+            }*/
         }
-        
+
     }
+}
